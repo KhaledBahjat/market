5c27fa2b2d0adb5ecd64019e69de4721a57ed149
Khaled <khaledbahjat0@gmail.com>
2026-05-26 07:59:20 -0700

    Refactor bottom navigation bar in MainHome with improved styling and color scheme

--- market/lib/features/auth/nav_bar/ui/main_home.dart
+++ market/lib/features/auth/nav_bar/ui/main_home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:market/core/theme/app_colors.dart';
 
 class MainHome extends StatelessWidget {
   const MainHome({super.key});
@@ -13,41 +15,34 @@ class MainHome extends StatelessWidget {
           child: Container(child: Text('Test')),
         ),
       ),
-      bottomNavigationBar: GNav(
-  rippleColor: Colors.grey[800]!, // tab button ripple color when pressed
-  hoverColor: Colors.grey[700]!, // tab button hover color
-  haptic: true, // haptic feedback
-  tabBorderRadius: 15, 
-  tabActiveBorder: Border.all(color: Colors.black, width: 1), // tab button border
-  tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
-  tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
-  curve: Curves.easeOutExpo, // tab animation curves
-  duration: Duration(milliseconds: 900), // tab animation duration
-  gap: 8, // the tab button gap between icon and text 
-  color: Colors.grey[800], // unselected icon color
-  activeColor: Colors.purple, // selected icon and text color
-  iconSize: 24, // tab button icon size
-  tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
-  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
-  tabs: [
-    GButton(
-      icon: Icons.home,
-      text: 'Home',
-    ),
-    GButton(
-      icon: Icons.store,
-      text: 'Store',
-    ),
-    GButton(
-      icon: Icons.favorite,
-      text: 'Favorite',
-    ),
-    GButton(
-      icon: Icons.person,
-      text: 'Profile',
-    )
-  ]
-),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.symmetric(vertical: 12.h,horizontal: 18.w),
+        child: GNav(
+          rippleColor:
+              AppColors.kPrimaryColor, 
+          hoverColor: AppColors.kPrimaryColor, 
+          haptic: true,
+          curve: Curves.easeOutExpo, 
+          duration: Duration(milliseconds: 400), 
+          gap: 8, 
+          color: Colors.grey,
+          activeColor: AppColors.kPrimaryColor,
+          iconSize: 24, 
+          tabBackgroundColor: AppColors.kPrimaryColor.withOpacity(
+            0.1,
+          ), 
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 12,
+          ), 
+          tabs: [
+            GButton(icon: Icons.home, text: 'Home'),
+            GButton(icon: Icons.store, text: 'Store'),
+            GButton(icon: Icons.favorite, text: 'Favorite'),
+            GButton(icon: Icons.person, text: 'Profile'),
+          ],
+        ),
+      ),
     );
   }
 }
