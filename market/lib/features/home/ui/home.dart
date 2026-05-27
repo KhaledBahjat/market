f1f59c80966deeea87139736f548f17b9b85f1e6
Khaled <khaledbahjat0@gmail.com>
2026-05-27 11:54:33 -0700

    Enhance Home screen layout with category icons and names; integrate AppColors for styling

--- market/lib/features/home/ui/home.dart
+++ market/lib/features/home/ui/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:market/core/helper/spacing.dart';
+import 'package:market/core/theme/app_colors.dart';
 import 'package:market/core/widgets/cousttom_search_feild.dart';
 
 class Home extends StatelessWidget {
@@ -13,10 +15,62 @@ class Home extends StatelessWidget {
         children: [
           CustomSearchFeild(),
           Height(height: 20),
-          Image.asset('assets/imgs/market.jpg',fit: BoxFit.cover,),
+          Image.asset('assets/imgs/market.jpg', fit: BoxFit.cover),
+          Height(height: 20),
+          const Text(
+            'Popular Categories',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Height(height: 10),
+          SizedBox(
+            height: 100.h,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: AppColors.kPrimaryColor,
+                      radius: 30,
+                      child: Icon(
+                        categories[index].icon,
+                        color: AppColors.kWhiteColor,
+                        size: 30,
+                      ),
+                    ),
+                    Height(height: 5),
+                    Text(
+                      categories[index].name,
+                      style:  TextStyle(fontSize: 15.sp),
+                    ),
+                  ],
+                ),
+              ),
+              itemCount: categories.length,
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
+List<CategoryItem> categories = [
+  CategoryItem(name: 'Phones', icon: Icons.phone_android),
+  CategoryItem(name: 'Laptops', icon: Icons.laptop),
+  CategoryItem(name: 'Headphones', icon: Icons.headphones),
+  CategoryItem(name: 'Cameras', icon: Icons.camera_alt),
+  CategoryItem(name: 'Smartwatches', icon: Icons.watch),
+  CategoryItem(name: 'Gaming Consoles', icon: Icons.videogame_asset),
+  CategoryItem(name: 'Tablets', icon: Icons.tablet_mac),
+  CategoryItem(name: 'Smartphones', icon: Icons.phone),
+  
+];
+
+class CategoryItem {
+  final String name;
+  final IconData icon;
+
+  CategoryItem({required this.name, required this.icon});
+}
