f4220cbf8c2581fa80f9c843c8a7afba0f265193
Khaled <khaledbahjat0@gmail.com>
2026-05-31 16:28:57 -0700

    Refactor Store widget to enhance layout and display product list

--- market/lib/features/store/ui/store.dart
+++ market/lib/features/store/ui/store.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:market/core/helper/spacing.dart';
+import 'package:market/core/widgets/cousttom_search_feild.dart';
+import 'package:market/core/widgets/proudct_list.dart';
 
 class Store extends StatelessWidget {
   const Store({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Store'),);
+    return  Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: [
+            Center(
+              child: const Text(
+              'Welcome to our store!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+            ),
+            Height(height: 10),
+          CustomSearchFeild(),
+          Height(height: 20),
+          ProudctList()
+        ],
+      ),
+    );
   }
 }
