770b235ed4a1f631e6684b31cec2d22ced427c98
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-06-17 18:52:24 +0300

    Refactor ProductDetailsCubit to improve average rating calculation and streamline product rate assignment

--- market/lib/features/proudct_details/logic/cubit/product_details_cubit.dart
+++ market/lib/features/proudct_details/logic/cubit/product_details_cubit.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:market/core/api_services.dart';
 import 'package:market/features/proudct_details/logic/models/rate_model.dart';
 import 'package:meta/meta.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 part 'product_details_state.dart';
 
@@ -12,6 +13,7 @@ class ProductDetailsCubit extends Cubit<ProductDetailsState> {
   final ApiServices _apiServices = ApiServices();
   List<RateModel> rates = [];
   int averageRate = 0;
+  int productRate = 5;
   Future<void> getProductRates({required String productId}) async {
     emit(GetProductRateLoading());
     try {
@@ -22,18 +24,25 @@ class ProductDetailsCubit extends Cubit<ProductDetailsState> {
           .map<RateModel>((item) => RateModel.fromJson(item))
           .toList();
 
-      for (var rate in rates) {
-        if(rate.rate != null){
-          averageRate += rate.rate!;
-        }
-        averageRate ~/= rates.length;
-      }
+      _getAvreageRate();
+      rates.where((userRate)=>userRate.forUser==Supabase.instance.client.auth.currentUser?.id).forEach((userRate) {
+        productRate=userRate.rate??5;
+      });
       log(averageRate.toString());
       // log(rates[0].rate.toString());
       emit(GetProductRateSuccess());
     } catch (e) {
       log(e.toString());
       emit(GetProductRateError(e.toString()));
+    }
+  }
+
+  void _getAvreageRate() {
+      for (var rate in rates) {
+      if(rate.rate != null){
+        averageRate += rate.rate!;
+      }
+      averageRate ~/= rates.length;
     }
   }
 }
